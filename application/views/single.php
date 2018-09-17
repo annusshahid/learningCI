9b32a1b0edf4768cd573c95b75415d42808f4acc
annusshahid <annusshahid@gmail.com>
2018-09-18 03:37:38 +0500

    after copying files

---
+++ application/views/single.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html> 
+<html lang = "en"> 
+
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>Single post view</title> 
+   </head>
+	
+   <body> 
+      Single post page 
+   </body>
+	
+</html>
